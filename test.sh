641865453cef9a69ab6c2497ce69e856209d2b85
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 13:46:49 +0000

    Working script for build

--- test.sh
+++ test.sh
@@ -16,9 +16,7 @@ echo "Looking for iamges to build"
 for dir in $(find ./lambda_functions -maxdepth 1 -mindepth 1 -type d ); do
 
     if [[ -f "$dir/build_if.sh" ]]; then
-        ${dir}/build_if.sh
-    else
-        echo "Nothing to update"    
+        ${dir}/build_if.sh  
     fi
 done
 
