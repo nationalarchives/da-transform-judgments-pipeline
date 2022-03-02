583986fa2ff640f2bde2970ebb6510b72f955ea6
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 13:44:55 +0000

    Working script for build

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ for dir in $(find ./lambda_functions -maxdepth 1 -mindepth 1 -type d ); do
     if [[ -f "$dir/build_if.sh" ]]; then
         ${dir}/build_if.sh
     else
-        "Nothing to update"    
+        echo "Nothing to update"    
     fi
 done
 
