02078e08662eb14eb3a158cab19f066b38b257d6
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 02:22:01 +0000

    added script to build image

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 
-if [[  $(git diff origin/test HEAD^^ lambda_functions/tdr_message/*.py) ]]; then
-	echo "Hello"
-else
-	echo "No"
-fi
+
+
+for dir in $(find ../da-transform-judgement-pipeline/lambda_functions -maxdepth 1 -mindepth 1 -type d ); do
+    if [[ -f "../da-transform-judgement-pipeline/lambda_functions/${dir}" ]]; then
+        if [[  $(git diff origin/test HEAD^^ ../da-transform-judgement-pipeline/lambda_functions/${dir}/*.py) ]]; then
+	        echo "Hello"
+        else
+	        echo "No"
+        fi
+    fi
+done
