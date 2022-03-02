9345d4430b0f4384f36def392c4059f34acd648c
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 15:28:26 +0000

    Working script for build

--- lambda_functions/tdr_message/build.sh
+++ lambda_functions/tdr_message/build.sh
@@ -4,16 +4,16 @@ set -e
 source_path=$(which $0)
 
 dir_name=$(dirname $source_path)
-
-$(basename $dir_name)
-
+echo $dir_name
+basename $dir_name
+image_name=$(basename $dir_name)
 account_id=`aws sts get-caller-identity --output text --query 'Account'`
 docker --version
-docker build -t $(basename $dir_name) .
+docker build -t $image_name $dir_name
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.eu-west-2.amazonaws.com
-output=$(aws ecr describe-repositories --repository-names $(basename $dir_name) --region eu-west-2 2>&1)
+output=$(aws ecr describe-repositories --repository-names $image_name --region eu-west-2 2>&1)
 if echo ${output} | grep -q RepositoryNotFoundException; then
-    aws ecr create-repository --repository-name $(basename $dir_name) --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+    aws ecr create-repository --repository-name $image_name --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
 else
     echo "Repository already exists" 
 fi
--- lambda_functions/tdr_message/build_if.sh
+++ lambda_functions/tdr_message/build_if.sh
@@ -12,8 +12,7 @@ dir_name=$(dirname $source_path)
 current_branch=$(git branch --show-current)
 
 if [[  $(git diff origin/test HEAD^ $dir_name/*.py) ]]; then
-    
-    ./build.sh
+    $dir_name/build.sh
 else
     echo "Nothing to update in $(basename $dir_name)"
 fi
