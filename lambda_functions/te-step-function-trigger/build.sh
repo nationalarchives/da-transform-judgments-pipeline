fe362b4809b5ecf783f3e2c90b9ef009572c8b8b
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 18:55:53 +0000

    update_fucntion script added

--- lambda_functions/te-step-function-trigger/build.sh
+++ lambda_functions/te-step-function-trigger/build.sh
@@ -2,24 +2,27 @@
 
 set -e
 source_path=$(which $0)
+echo $source_path
 
 dir_name=$(dirname $source_path)
+
+echo $dir_name
 image_name=$(basename $dir_name)
 account_id=`aws sts get-caller-identity --output text --query 'Account'`
 docker --version
-docker build -t $image_name $dir_name
+docker build -t lambda_functions/$image_name $dir_name
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.eu-west-2.amazonaws.com
 
-if [[ $(aws ecr describe-repositories --repository-names $image_name --region eu-west-2) ]]; then
+if [[ $(aws ecr describe-repositories --repository-names lambda_functions/$image_name --region eu-west-2) ]]; then
     echo "Repository already exists" 
 else
-    aws ecr create-repository --repository-name $image_name --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE  
+    aws ecr create-repository --repository-name lambda_functions/$image_name --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE  
 fi
 
 # docker tag  $image_name:latest $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:`date`
-docker tag  $image_name:latest $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:latest
+docker tag  lambda_functions/$image_name:latest $account_id.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/$image_name:latest
 # docker push $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:`date` 
-docker push $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:latest    
+docker push $account_id.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/$image_name:latest    
 
 exit
 
--- lambda_functions/te-step-function-trigger/update_function.sh
+++ lambda_functions/te-step-function-trigger/update_function.sh
@@ -9,4 +9,4 @@ dir_name=$(dirname $source_path)
 image_name=$(basename $dir_name)
 account_id=`aws sts get-caller-identity --output text --query 'Account'`
 
-aws lambda update-function-code --function-name dev-tdr-sqs-message --image-uri $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:latest
+aws lambda update-function-code --function-name dev-tdr-sqs-message --image-uri $account_id.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/$image_name:latest
