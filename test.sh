aa95c2d2ef432929850a51c9949567de69bc20ac
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 02:45:07 +0000

    added script to build image

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+set -e
 
-
+git diff origin/test HEAD^ lambda_functions/tdr_message/*.py
 
 for dir in $(find ./lambda_functions -maxdepth 1 -mindepth 1 -type d ); do
-    if [[  $(git diff origin/test HEAD^^ lambda_functions/${dir}/*.py) ]]; then
+    if [[  $(git diff origin/test HEAD^ lambda_functions/${dir}/*.py) ]]; then
         echo "Building a new image in ${dir}"
     else
         echo "Nothing to update in ${dir}"
     fi
 done
+
+exit
