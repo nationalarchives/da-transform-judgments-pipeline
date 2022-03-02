96af3e833a06b1ddba48d5dd402db853fe46a7c7
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 16:29:46 +0000

    improved script

--- lambda_functions/tdr_message/build.sh
+++ lambda_functions/tdr_message/build.sh
@@ -12,14 +12,11 @@ docker --version
 docker build -t $image_name $dir_name
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.eu-west-2.amazonaws.com
 if [ $(aws ecr describe-repositories --repository-names $image_name --region eu-west-2 | grep -q RepositoryNotFoundException)]; then
-    if echo ${output} | grep -q RepositoryNotFoundException; then
-        aws ecr create-repository --repository-name $image_name --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
-    else
-        echo "Repository already exists" 
-    fi
+    aws ecr create-repository --repository-name $image_name --region eu-west-2 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
 else
-    echo "repo"
+    echo "Repository already exists" 
 fi
+
 
 
 exit
