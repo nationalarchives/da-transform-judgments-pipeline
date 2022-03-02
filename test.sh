401c7736ada6bcf59ca02c15e0bb30a457ea324c
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 02:36:09 +0000

    added script to build image

--- test.sh
+++ test.sh
@@ -4,11 +4,9 @@
 
 
 for dir in $(find ./lambda_functions -maxdepth 1 -mindepth 1 -type d ); do
-    if [[ -f "./lambda_functions/${dir}/*.py" ]]; then
-        if [[  $(git diff origin/test HEAD^^ lambda_functions/${dir}/*.py) ]]; then
-	        echo "Hello"
-        else
-	        echo "No"
-        fi
+    if [[  $(git diff origin/test HEAD^^ lambda_functions/${dir}/*.py) ]]; then
+        echo "Hello"
+    else
+        echo "No"
     fi
 done
