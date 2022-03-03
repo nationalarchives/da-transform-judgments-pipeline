99ed2aad703276935b11a481729a2954b7ce546a
rahmedzaizi <rahmed@zaizi.com>
2022-03-03 15:20:53 +0000

    added scripts for other functions

--- lambda_functions/bagit-checksum-validation/build_if.sh
+++ lambda_functions/bagit-checksum-validation/build_if.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-set -e
-# iamge_name=$(cd `dirname $0` && pwd)
-# echo $image_name
-source_path=$(which $0)
+# set -e
+# # iamge_name=$(cd `dirname $0` && pwd)
+# # echo $image_name
+# source_path=$(which $0)
 
-dir_name=$(dirname $source_path)
+# dir_name=$(dirname $source_path)
 
 
 
-# current_branch=$(git branch --show-current)
+# # current_branch=$(git branch --show-current)
 
-if [[  $(git diff origin/test HEAD^ $dir_name/*.py) ]]; then
+# if [[  $(git diff origin/test HEAD^ $dir_name/*.py) ]]; then
     
-    echo "Building $(basename $dir_name) Image"
-else
-    echo "Nothing to update in $(basename $dir_name)"
-fi
+#     echo "Building $(basename $dir_name) Image"
+# else
+#     echo "Nothing to update in $(basename $dir_name)"
+# fi
 
-exit
+# exit
