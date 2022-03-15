eb5e9daff73373a390b6afc7500b9347a90adc32
Colin Bowen <cbowen@zaizi.com>
2022-03-15 11:40:20 +0000

    Formatting, tmp dir fix, error messages

--- lambda_functions/te-parser-function/te_text_parser.py
+++ lambda_functions/te-parser-function/te_text_parser.py
@@ -8,8 +8,7 @@ import json
 
 # Set global logging options; AWS environment may override this though
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 # Instantiate logger
@@ -17,18 +16,20 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # move to env var
-API_ENDPOINT = "https://7gwnzr88s0.execute-api.eu-west-2.amazonaws.com/default/dev-te-text-parser"
+API_ENDPOINT = (
+    "https://7gwnzr88s0.execute-api.eu-west-2.amazonaws.com/default/dev-te-text-parser"
+)
 API_KEY = ""
 
-KEY_ERROR='error'
-KEY_ERROR_MESSAGE='error-message'
-KEY_S3_BUCKET='s3-bucket'
-KEY_S3_PARSER_BUCKET='dev-te-judgment-out'
-KEY_S3_BAGIT_NAME='s3-bagit-name'
-KEY_S3_OBJECT_ROOT='s3-object-root'
-KEY_PARSED_FILES='files'
-KEY_OUTPUT_MESSAGE='output-message'
-KEY_NUM_RETRIES='number-of-retries'
+KEY_ERROR = "error"
+KEY_ERROR_MESSAGE = "error-message"
+KEY_S3_BUCKET = "s3-bucket"
+KEY_S3_PARSER_BUCKET = "dev-te-judgment-out"
+KEY_S3_BAGIT_NAME = "s3-bagit-name"
+KEY_S3_OBJECT_ROOT = "s3-object-root"
+KEY_PARSED_FILES = "parsed-files"
+KEY_OUTPUT_MESSAGE = "output-message"
+KEY_NUM_RETRIES = "number-of-retries"
 
 
 def handler(event, context):
@@ -103,20 +104,20 @@ def handler(event, context):
 
     try:
         # Get input parameters
-        s3_bucket = event['s3-bucket']
+        s3_bucket = event["s3-bucket"]
         output[KEY_S3_BUCKET] = s3_bucket
-        s3_bagit_name = event['s3-bagit-name']
+        s3_bagit_name = event["s3-bagit-name"]
         logger.info(f's3_bucket="{s3_bucket}" s3_bagit_name="{s3_bagit_name}"')
         output[KEY_S3_BAGIT_NAME] = s3_bagit_name
         # Forward prior output-message
         output[KEY_OUTPUT_MESSAGE] = event[KEY_OUTPUT_MESSAGE].copy()
 
         # get document from s3
-        S3 = boto3.client('s3')
-        S3_resource = boto3.resource('s3')
+        S3 = boto3.client("s3")
+        S3_resource = boto3.resource("s3")
         object_key = event.get("validated-files").get("data")[0]
         s3_object = S3.get_object(Bucket=s3_bucket, Key=object_key)
-        document = s3_object['Body'].read()
+        document = s3_object["Body"].read()
 
         # encode body to base64
         encoded = base64.b64encode(document).decode("utf-8")
@@ -127,64 +128,97 @@ def handler(event, context):
 
         # send request to parser
         headers = {"Content-Type": "application/json", "Accept": "application/json"}
-        response = requests.request("POST", API_ENDPOINT, headers=headers, data=json.dumps(data))
+        response = requests.request(
+            "POST", API_ENDPOINT, headers=headers, data=json.dumps(data)
+        )
         response_json = response.json()
         logger.info("response")
 
         output_obj = {
             "xml": response_json.get("xml", None),
             "meta": {
-                "uri" : response_json.get("meta").get("uri", None),
+                "uri": response_json.get("meta").get("uri", None),
                 "court": response_json.get("meta").get("court", None),
                 "cite": response_json.get("meta").get("cite", None),
                 "date": response_json.get("meta").get("date", None),
                 "name": response_json.get("meta").get("name", None),
-                "attachments": response_json.get("meta").get("attachments", None)
+                "attachments": response_json.get("meta").get("attachments", None),
             },
             "images": response_json.get("images", None),
         }
 
+        os.mkdir("tmp")
         # xml
-        with open(f'tmp/{event["output-message"]["consignment-reference"]}-te-xml.xml', 'w') as xml_file:
+        with open(
+            f'tmp/{event["output-message"]["consignment-reference"]}-te-xml.xml', "w"
+        ) as xml_file:
             xml_file.write(output_obj["xml"])
             object_key = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-xml.xml"
-            S3.upload_file(f'tmp/{event["output-message"]["consignment-reference"]}-te-xml.xml', KEY_S3_PARSER_BUCKET, object_key)
+            S3.upload_file(
+                f'tmp/{event["output-message"]["consignment-reference"]}-te-xml.xml',
+                KEY_S3_PARSER_BUCKET,
+                object_key,
+            )
 
         # meta
-        with open(f'tmp/{event["output-message"]["consignment-reference"]}-te-meta.json', 'w') as json_file:
+        with open(
+            f'tmp/{event["output-message"]["consignment-reference"]}-te-meta.json', "w"
+        ) as json_file:
             json_file.write(json.dumps(output_obj["meta"]))
             object_key = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-meta.json"
-            S3.upload_file(f'tmp/{event["output-message"]["consignment-reference"]}-te-meta.json', KEY_S3_PARSER_BUCKET, object_key)
+            S3.upload_file(
+                f'tmp/{event["output-message"]["consignment-reference"]}-te-meta.json',
+                KEY_S3_PARSER_BUCKET,
+                object_key,
+            )
 
-        # judgement
+        #  judgement
         source = {
             "Bucket": s3_bucket,
-            "Key": event.get("validated-files").get("data")[0]
+            "Key": event.get("validated-files").get("data")[0],
         }
         dest = S3_resource.Bucket(KEY_S3_PARSER_BUCKET)
-        dest.copy(source, f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/judgment.docx")
+        dest.copy(
+            source,
+            f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/judgment.docx",
+        )
 
         # bagit-info
         source = {
             "Bucket": s3_bucket,
-            "Key": f"{event.get('validated-files').get('path')}/bag-info.txt"
+            "Key": f"{event.get('validated-files').get('path')}/bag-info.txt",
         }
         dest = S3_resource.Bucket(KEY_S3_PARSER_BUCKET)
-        dest.copy(source, f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/bagit-info.txt")
+        dest.copy(
+            source,
+            f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/bagit-info.txt",
+        )
 
-        
         # place xml, meta json, bag it info in output message
-        output["consignment-reference"] = event["output-message"]["consignment-reference"]
-        output["s3-object-root"] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}"
+        output["consignment-reference"] = event["output-message"][
+            "consignment-reference"
+        ]
+        output[
+            "s3-object-root"
+        ] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}"
         output["s3-parser-bucket"] = KEY_S3_PARSER_BUCKET
-        output[KEY_PARSED_FILES]["xml"] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-xml.xml"
-        output[KEY_PARSED_FILES]["judgement"] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/judgement.docx"
-        output[KEY_PARSED_FILES]["meta"] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-meta.json"
-        output[KEY_PARSED_FILES]["bagit-info"] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/bagit-info.txt"
-
+        output[KEY_PARSED_FILES][
+            "xml"
+        ] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-xml.xml"
+        output[KEY_PARSED_FILES][
+            "judgement"
+        ] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/judgement.docx"
+        output[KEY_PARSED_FILES][
+            "meta"
+        ] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/te-meta.json"
+        output[KEY_PARSED_FILES][
+            "bagit-info"
+        ] = f"parsed/judgement/{event['output-message']['consignment-reference']}/{event['output-message']['number-of-retries']}/bagit-info.txt"
 
     except Exception as e:
+        output[KEY_ERROR] = True
+        output[KEY_ERROR_MESSAGE] = f"{e}"
         logger.error(f"Error - {e}")
 
-    logger.info('handler return')
+    logger.info("handler return")
     return output
