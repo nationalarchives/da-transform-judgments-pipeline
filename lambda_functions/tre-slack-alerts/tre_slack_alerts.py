6f55372ee1695067ff4fb3cb3b6405e2df381eef
Colin Bowen <cbowen@zaizi.com>
2022-04-28 16:52:58 +0100

    Edit status messages, add warning

--- lambda_functions/tre-slack-alerts/tre_slack_alerts.py
+++ lambda_functions/tre-slack-alerts/tre_slack_alerts.py
@@ -5,7 +5,7 @@ import os
 http = urllib3.PoolManager()
 
 
-def lambda_handler(event):
+def lambda_handler(event, context):
     print(event)
     url = os.environ["SLACK_WEBHOOK_URL"]
     env = os.environ["ENV"]
@@ -17,26 +17,31 @@ def lambda_handler(event):
     if "ErrorMessage" in message:
         error_type = message["ErrorType"]
         error_message = message["ErrorMessage"]
-        final_message = f"*ERROR* :alert: <!here> \n*ENVIRONMENT* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n*ErrorType* `{error_type}` \n``` Error Message: {str(error_message)} ``` \n"
+        final_message = f"*STATUS* :alert: <!here> \n*Environment* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n*ErrorType* `{error_type}` \n``` Error Message: {str(error_message)} ``` \n"
 
         msg = {
             "channel": os.environ["SLACK_CHANNEL"],
             "username": os.environ["SLACK_USERNAME"],
             "text": final_message,
-            "icon_emoji": ":x:",
+            "icon_emoji": ":red_circle:",
         }
     else:
-        final_message = f"*SUCCESS* :white_check_mark: \n  *ENVIRONMENT* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n"
+        final_message = f"*STATUS* :white_check_mark: \n  *Environment* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n"
         if "Type" in message:
             message_type = message["Type"]
             if str(message_type) == "retry":
-                print("Editorial Retry Message received")
-                icon = ":retry:"
-                final_message = f"*SUCCESS* {icon} \n*ENVIRONMENT* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n*Type* `EditorialRetry` \n"
-            elif str(message_type) == "tda":
+                print("Retry Message received")
+                icon = ":large_green_circle:"
+                final_message = f"*STATUS* {icon} \n*Environment* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n"
+            elif str(message_type) == "message":
                 print("TDA Message received")
-                icon = ":white_check_mark:"
-                final_message = f"*SUCCESS* {icon} \n*ENVIRONMENT* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n"
+                icon = ":large_green_circle:"
+                final_message = f"*STATUS* {icon} \n*Environment* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n"
+            elif str(message_type) == "warning":
+                print("Warning Message received")
+                icon = ":large_orange_circle:"
+                warning_message = message["WarningMessage"]
+                final_message = f"*STATUS* {icon} \n*Environment* `{env}` \n*ExecutionName* `{execution}` \n*StateMachine* `{state_machine}` \n *ErrorMessage* `{warning_message}`"
 
             msg = {
                 "channel": os.environ["SLACK_CHANNEL"],
