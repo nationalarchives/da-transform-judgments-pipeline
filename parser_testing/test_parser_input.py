8284c197e2388849eaaa170b0a5d4b0212b6a3f9
rahmedzaizi <rahmed@zaizi.com>
2022-05-17 13:40:31 +0100

    parser test script added

--- parser_testing/test_parser_input.py
+++ parser_testing/test_parser_input.py
@@ -1,18 +1,9 @@
-from http import client
 import json
 import logging
-from urllib import response
 import boto3
 from botocore.exceptions import ClientError
 
 def create_presigned_url(bucket_name, object_name, expiration=3600):
-    """Generate a presigned URL to share an S3 object
-
-    :param bucket_name: string
-    :param object_name: string
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Presigned URL as string. If error, returns None.
-    """
     bucket_name = "dev-te-testdata"
     object_name = "parser_test_docs/test.docx"
     # Generate a presigned URL for the S3 object
