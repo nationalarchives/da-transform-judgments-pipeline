c5d618c9f0c85fdb3b4bbdf32738138d69b8608b
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-07-13 16:12:25 +0100

    TE prefixes to TRE; bump image versions

--- testing/te_bagit_then_files/test-bagit-then-files.py
+++ testing/te_bagit_then_files/test-bagit-then-files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import te_bagit_checksum_validation
-import te_files_checksum_validation
+import tre_bagit_checksum_validation
+import tre_files_checksum_validation
 import sys
 import json
 
@@ -10,11 +10,11 @@ if len(sys.argv) != 2:
 event=sys.argv[1]
 context=None
 
-output_bagit = te_bagit_checksum_validation.handler(json.loads(event), context)
-print(f'te_bagit_checksum_validation output:\n{json.dumps(output_bagit, indent=4)}')
+output_bagit = tre_bagit_checksum_validation.handler(json.loads(event), context)
+print(f'tre_bagit_checksum_validation output:\n{json.dumps(output_bagit, indent=4)}')
 
 if str(output_bagit['error']).lower() != 'false':
     raise ValueError(output_bagit['error-message'])
 
-output_files = te_files_checksum_validation.handler(output_bagit, context)
-print(f'te_files_checksum_validation output:\n{json.dumps(output_files, indent=4)}')
+output_files = tre_files_checksum_validation.handler(output_bagit, context)
+print(f'tre_files_checksum_validation output:\n{json.dumps(output_files, indent=4)}')
