b80e856bb30ac2036512c794c0be467fafeedf3e
rahmedzaizi <rahmed@zaizi.com>
2022-08-03 19:21:31 +0100

    pretified log printings

--- lambda_functions/tre-rapb-trigger/tre_rapb_trigger.py
+++ lambda_functions/tre-rapb-trigger/tre_rapb_trigger.py
@@ -2,21 +2,19 @@ import json
 import boto3
 import uuid
 import os
-import pprint
 
 
 client = boto3.client('stepfunctions')
 STATE_MACHINE_ARN = os.environ['RAPB_ARN']
 def lambda_handler(event, context):
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(event)
-    pp.pprint(event['Records'])
+    print(f" Event: {(json.dumps(event, indent=2))}")
     record = event['Records'][0]
+    print(f"Records: {json.dumps(record, indent=2)}")
     body = json.loads(record['body'])
-    pp.pprint(body)
+    print(f"Body: {(json.dumps(body, indent=2))}")
     message = json.loads(body['Message'])
-    pp.pprint(message)
-    print(message['consignment-reference'])
+    print(f"Message: {(json.dumps(message, indent=2))}")
+    print(f" Consignment-Reference: {(message['consignment-reference'])}")
     prefix = ""
     if 'consignment-reference' in message:
         prefix = message['consignment-reference']
@@ -39,9 +37,9 @@ def lambda_handler(event, context):
     unique_id = uuid.uuid4().hex
 
     name = "tre-" + prefix + "-" + retry + "-" + event_source + "-" + unique_id
-
+    
     response = client.start_execution(
-		stateMachineArn = STATE_MACHINE_ARN,
-		name = name,
-		input = json.dumps(record)
-		)
+        stateMachineArn = STATE_MACHINE_ARN,
+        name = name,
+        input = json.dumps(message)
+    )
