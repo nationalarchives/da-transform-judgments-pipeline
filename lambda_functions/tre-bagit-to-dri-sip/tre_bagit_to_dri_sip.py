086e9d89c3230f2e159a8f1b42a5b149f21131ee
Mark Kingsbury <mark@kingsburydata.com>
2022-08-19 13:13:16 +0100

    update comment for buckets used

--- lambda_functions/tre-bagit-to-dri-sip/tre_bagit_to_dri_sip.py
+++ lambda_functions/tre-bagit-to-dri-sip/tre_bagit_to_dri_sip.py
@@ -31,8 +31,8 @@ KEY_OUTPUT_MESSAGE='output-message'
 
 def handler(event, context):
     """
-    Given a bagit unzip sitting in the temp-bucket determined by env var and with location built from the event's
-    type/consignemnt_ref/retry_number then a dri-sip is built in the same temp-bucket at
+    Given a bagit unzip sitting in the s3-bucket in event and with location built from the event's
+    type/consignemnt_ref/retry_number then a dri-sip is built in bucket provided in env variable S3_DRI_OUT_BUCKET at
     type/consignemnt_ref/retry_number/sip (and a zip + checksum of that zip are placed next to it)
     """
     logger.info(f'handler start: event="{event}"')
