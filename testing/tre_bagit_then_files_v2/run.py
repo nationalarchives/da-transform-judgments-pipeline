7d63f8e0ebd286cdaaa879b853b577e298c6f9ce
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-08-22 10:30:11 +0100

    Adapt test to new lambda names

--- testing/tre_bagit_then_files_v2/run.py
+++ testing/tre_bagit_then_files_v2/run.py
@@ -3,8 +3,8 @@
 Module to run tre_validate_bagit then tre_validate_bagit_files.
 See corresponding run.sh file.
 """
-import tre_validate_bagit
-import tre_validate_bagit_files
+import tre_vb_validate_bagit
+import tre_vb_validate_bagit_files
 import sys
 import json
 
@@ -14,8 +14,8 @@ if len(sys.argv) != 2:
 event=json.loads(sys.argv[1])
 context=None
 
-result_1 = tre_validate_bagit.handler(event=event, context=context)
+result_1 = tre_vb_validate_bagit.handler(event=event, context=context)
 print(f'tre_validate_bagit output:\n{json.dumps(result_1, indent=2)}')
 
-result_2 = tre_validate_bagit_files.handler(event=result_1, context=context)
+result_2 = tre_vb_validate_bagit_files.handler(event=result_1, context=context)
 print(f'tre_validate_bagit_files output:\n{json.dumps(result_2, indent=2)}')
