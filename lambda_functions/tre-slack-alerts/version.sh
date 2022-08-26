94de9d01ba347c7e1c7025e51773f8ea246c4c8d
rahmedzaizi <rahmed@zaizi.com>
2022-08-26 11:30:43 +0100

    incremented lambda versions

--- lambda_functions/tre-slack-alerts/version.sh
+++ lambda_functions/tre-slack-alerts/version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker_image_name=tre-slack-alerts
-docker_image_tag=0.0.25
+docker_image_tag=0.0.26
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
--- lambda_functions/tre-step-function-trigger/version.sh
+++ lambda_functions/tre-step-function-trigger/version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker_image_name=tre-step-function-trigger
-docker_image_tag=0.0.8
+docker_image_tag=0.0.9
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
