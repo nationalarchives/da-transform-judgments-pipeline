23f9fa2a6a881d5dc95b8fceb4177c9dcfc3f491
rahmedzaizi <rahmed@zaizi.com>
2022-09-02 11:20:51 +0100

    tre-forward function added

---
+++ lambda_functions/tre-forward/tre_forward.py
@@ -0,0 +1,17 @@
+from email import message
+import boto3
+import json
+import os
+
+sns = boto3.client('sns')
+tre_out_topic_arn = os.environ['TRE_OUT_TOPIC_ARN']
+
+def lambda_handler(event, context):
+    record = event['Record'][0]
+    message = json.loads(record['body']['Message'])
+
+    response = sns.publish(
+        TopicArn = tre_out_topic_arn,
+        Message = json.dumps(message)
+    )
+    print(response)
