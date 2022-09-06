1bec4193d5f7da4cbf01a9e6f72df8c6b3915a83
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-09-06 13:45:48 +0100

    Incorporate tre_event_lib; add output error event

--- lambda_functions/tre-vb-validate-bagit-files/version.sh
+++ lambda_functions/tre-vb-validate-bagit-files/version.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 docker_image_name=tre-vb-validate-bagit-files
-docker_image_tag=2.0.3
+docker_image_tag=2.0.4
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
 # shellcheck disable=SC2034  # var imported elsewhere
-lib_build_list=('s3_lib' 'lib/tre_lib')
+lib_build_list=('s3_lib')
+# shellcheck disable=SC2034  # var imported elsewhere
+tre_event_lib_build_tag=0.0.3-alpha
