be4ef2edab609063249096f42bd17614a9ea0d91
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-09-06 08:19:54 +0100

    Incorporate tre_event_lib; add output error event

--- lambda_functions/tre-vb-validate-bagit/tre_vb_validate_bagit.py
+++ lambda_functions/tre-vb-validate-bagit/tre_vb_validate_bagit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+"""
+Lambda handler for validate-bagit Step Function step.
+"""
 import logging
+import os
+from urllib.parse import urlparse
 from s3_lib import common_lib
 from s3_lib import checksum_lib
 from s3_lib import object_lib
-from urllib.parse import urlparse
-import os
-from tre_lib.message import Message
-from jsonschema import validate
-
+from tre_event_lib import tre_event_api
 
 # Set global logging options; AWS environment may override this though
 logging.basicConfig(
@@ -29,135 +30,62 @@ env_process = common_lib.get_env_var(
 env_environment = common_lib.get_env_var(
     'TRE_ENVIRONMENT', must_exist=True, must_have_value=True)
 
-OUTPUT_EVENT_NAME = 'bagit-received'
+EVENT_NAME_INPUT = 'bagit-available'
+EVENT_NAME_OUTPUT_OK = 'bagit-received'
+EVENT_NAME_OUTPUT_ERROR = 'bagit-validation-error'
 
-KEY_ERRORS = 'errors'
-KEY_ERROR = 'error'
-KEY_S3_BUCKET = 's3-bucket'
-KEY_NUM_RETRIES = 'number-of-retries'
 KEY_REFERENCE = 'reference'
 KEY_RESOURCE = 'resource'
 KEY_RESOURCE_VALIDATION = 'resource-validation'
 KEY_VALUE = 'value'
-KEY_PRODUCER = 'producer'
-KEY_TYPE = 'type'
+KEY_ERRORS = 'errors'
+KEY_S3_BUCKET = 's3-bucket'
 KEY_S3_BAGIT_NAME = 's3-bagit-name'
-
-
-def validate_input(event: dict):
-    """
-    Validate the input event using JSON schema.
-    """
-    logger.info(f'validating event against message schema')
-    schema_message = Message.get_schema()
-    logger.info(f'schema={schema_message}')
-    validate(instance=event, schema=schema_message)
-    logger.info(f'event schema validation OK')
-
-    schema_parameters_tdr = Message.get_schema('schema_param_tdr_to_tre.json')
-    logger.info(f'validating event parameters against expected schema')
-    logger.info(f'schema={schema_parameters_tdr}')
-    validate(
-        instance=event[Message.KEY_PARAMETERS],
-        schema=schema_parameters_tdr)
-    logger.info(f'event parameter schema validation OK')
-
-
-def validate_output(output_message_dict: dict):
-    """
-    Validate the output message using JSON schema.
-    """
-    logger.info(f'validating output_message against schema')
-    logger.info(f'output_message_dict={output_message_dict}')
-    validate(output_message_dict, Message.get_schema())
-    logger.info(f'output_message schema validation OK')
-    logger.info(f'validating output parameters against schema')
-    schema_param_tre = Message.get_schema(
-        'schema_param_tre_validate_bagit.json')
-    logger.info(f'schema={schema_param_tre}')
-    parameters = output_message_dict[Message.KEY_PARAMETERS]
-    logger.info(f'parameters={parameters}')
-    validate(parameters, schema_param_tre)
-    logger.info(f'output parameters schema validation OK')
 
 
 def handler(event, context):
     """
-    Copy the content of the input URLs in `event` fields `s3-bagit-url` and
-    `s3-sha-url` to s3 objects in the bucket name in environment variable
-    `TRE_S3_BUCKET`; the following prefix is used for the s3 objects:
+    Saves the BagIt and checksum files in the input's pre-signed URLs to S3
+    and validates the BagIt's checksum matches the checksum file.
 
-    * consignments/{consignment_type}/{consignment_reference}/{retry_count}
+    Expected Input:
+    * A `bagit-available` event
 
-    The following checks are performed:
-
-    * S3 files do not already exist
-    * Only 1 checksum is present in the manifest
-    * The 1 manifest checksum matches the corresponding s3 file
-
-    Ref: https://github.com/nationalarchives/da-transform-dev-documentation/blob/master/architecture-decision-records/001-Enhanced-message-structure.md
+    Output:
+    * A `bagit-received` event if validation is successful
+    * A `bagit-validation-error` event if validation fails
     """
     logger.info('handler start')
-    logger.info(f'type(event)="{type(event)}')
-    logger.info(f'event="{event}"')
-    validate_input(event=event)
-    logger.info('configuring initial output message')
+    logger.info(f'type(event)="%s', type(event))
+    logger.info(f'event:\n%s\n', event)
+    tre_event_api.validate_event(event=event, schema_name=EVENT_NAME_INPUT)
 
-    # Get required values from input event's parameters block
-    input_event_name = event[Message.KEY_PRODUCER][Message.KEY_EVENT_NAME]
-    logger.info(f'input_event_name={input_event_name}')
-    tdr_params = event[Message.KEY_PARAMETERS][input_event_name]
-    consignment_reference = tdr_params[KEY_REFERENCE]
-    s3_bagit_url = tdr_params[KEY_RESOURCE][KEY_VALUE]
-    s3_sha_url = tdr_params[KEY_RESOURCE_VALIDATION][KEY_VALUE]
-    consignment_type = event[KEY_PRODUCER][KEY_TYPE]
-    retry_count = int(tdr_params[KEY_NUM_RETRIES])
-    error_list = []
-
-    # Setup initial output parameters block fields
-    output_parameter_block = {
-        OUTPUT_EVENT_NAME: {
-            KEY_REFERENCE: consignment_reference,
-            KEY_S3_BUCKET: env_output_bucket,
-            KEY_NUM_RETRIES: retry_count,
-            KEY_ERRORS: error_list
-        }
-    }
-
-    # Output message
-    output_message = Message(
-        producer=env_producer,
-        process=env_process,
-        event_name=OUTPUT_EVENT_NAME,
-        environment=env_environment,
-        type=consignment_type,
-        prior_message=event,
-        parameters=output_parameter_block)
-
-    logger.info(f'output_message={output_message.to_json_str(indent=2)}')
+    # Get required values from input event's parameter block
+    input_params = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT]
+    consignment_reference = input_params[KEY_REFERENCE]
+    s3_bagit_url = input_params[KEY_RESOURCE][KEY_VALUE]
+    s3_sha_url = input_params[KEY_RESOURCE_VALIDATION][KEY_VALUE]
+    consignment_type = event[tre_event_api.KEY_PRODUCER][tre_event_api.KEY_TYPE]
+    # Get latest (last) UUID value from UUIDs list (list of dict)
+    event_uuid = list(event[tre_event_api.KEY_UUIDS][-1].values())[0]
+    logger.info(f'event_uuid=%s\n', event_uuid)
 
     try:
         # Determine output object path prefix
         output_object_prefix = (
             f'consignments/{consignment_type}/{consignment_reference}/'
-            f'{retry_count}')
+            f'{event_uuid}')
+        logger.info(f'output_object_prefix=%s\n', output_object_prefix)
 
         # Determine target s3 object names from the corresponding input URLs
         bagit_name = os.path.basename(urlparse(s3_bagit_url).path)
+        logger.info(f'bagit_name=%s\n', bagit_name)
         s3_bagit_name = f'{output_object_prefix}/{bagit_name}'
-        output_parameter_block[OUTPUT_EVENT_NAME][KEY_S3_BAGIT_NAME] = s3_bagit_name
+        logger.info(f's3_bagit_name=%s\n', s3_bagit_name)
         sha_name = os.path.basename(urlparse(s3_sha_url).path)
+        logger.info(f'sha_name=%s\n', sha_name)
         s3_sha_name = f'{output_object_prefix}/{sha_name}'
-
-        logger.info(
-            f'consignment_reference="{consignment_reference}" '
-            f's3_bagit_url="{s3_bagit_url}" s3_sha_url="{s3_sha_url}" '
-            f'consignment_type="{consignment_type}" '
-            f'retry_count="{retry_count}" '
-            f'bagit_name="{bagit_name}" sha_name="{sha_name}" '
-            f's3_bagit_name="{s3_bagit_name}" s3_sha_name="{s3_sha_name}" '
-            f'env_output_bucket="{env_output_bucket}"'
-            f'output_object_prefix="{output_object_prefix}"')
+        logger.info(f's3_sha_name=%s\n', s3_sha_name)
 
         # Copy files
         logger.info(f'Copy "{s3_bagit_url}" to "{s3_bagit_name}" '
@@ -195,11 +123,42 @@ def handler(event, context):
             env_output_bucket,
             s3_bagit_name,
             expected_checksum)
-    except ValueError as e:
-        logging.error(f'handler error: {str(e)}')
-        error_list.append({KEY_ERROR: str(e)})
-        output_parameter_block[OUTPUT_EVENT_NAME][KEY_NUM_RETRIES] = retry_count + 1
 
-    validate_output(output_message.to_dict())
-    logger.info('handler completed OK')
-    return output_message.to_dict()
+        output_parameter_block = {
+            EVENT_NAME_OUTPUT_OK: {
+                KEY_REFERENCE: consignment_reference,
+                KEY_S3_BUCKET: env_output_bucket,
+                KEY_S3_BAGIT_NAME: s3_bagit_name
+            }
+        }
+
+        event_output_ok = tre_event_api.create_event(
+            environment=env_environment,
+            producer=env_producer,
+            process=env_process,
+            event_name=EVENT_NAME_OUTPUT_OK,
+            prior_event=event,
+            parameters=output_parameter_block)
+        
+        logger.info(f'event_output_ok:\n%s\n', event_output_ok)
+        return event_output_ok
+    except ValueError as e:
+        logging.error(f'handler error: %s', str(e))
+        output_parameter_block = {
+            EVENT_NAME_OUTPUT_ERROR: {
+                KEY_REFERENCE: consignment_reference,
+                KEY_ERRORS: [str(e)]
+            }
+        }
+
+        event_output_error = tre_event_api.create_event(
+            environment=env_environment,
+            producer=env_producer,
+            process=env_process,
+            event_name=EVENT_NAME_OUTPUT_ERROR,
+            prior_event=event,
+            parameters=output_parameter_block
+        )
+
+        logger.info(f'event_output_error:\n%s\n', event_output_error)
+        return event_output_error
