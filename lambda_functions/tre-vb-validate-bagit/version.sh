410e71f13541f4c264686270f62124adab3403a5
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-09-06 13:46:27 +0100

    Update tre_event_lib version

--- lambda_functions/tre-vb-validate-bagit/version.sh
+++ lambda_functions/tre-vb-validate-bagit/version.sh
@@ -6,4 +6,4 @@ docker_image="${docker_image_name}":"${docker_image_tag}"
 # shellcheck disable=SC2034  # var imported elsewhere
 lib_build_list=('s3_lib')
 # shellcheck disable=SC2034  # var imported elsewhere
-tre_event_lib_build_tag=0.0.2-alpha
+tre_event_lib_build_tag=0.0.3-alpha
