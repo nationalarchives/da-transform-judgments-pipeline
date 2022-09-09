db917b11f8d4e1c9979c9ecd861aeeb977aa14c1
rahmedzaizi <rahmed@zaizi.com>
2022-09-09 11:42:06 +0100

    fixed key errors

--- lambda_functions/tre-forward/version.sh
+++ lambda_functions/tre-forward/version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker_image_name=tre-forward
-docker_image_tag=2.0.4
+docker_image_tag=2.0.5
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
