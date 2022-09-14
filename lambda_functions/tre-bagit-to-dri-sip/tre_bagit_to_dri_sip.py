5bc1c8d003bc6aabaa03f78e6fd27b87f9d75a7c
Mark Kingsbury <mark@kingsburydata.com>
2022-09-14 15:51:16 +0100

    DTE-310: input/output event updates

--- lambda_functions/tre-bagit-to-dri-sip/tre_bagit_to_dri_sip.py
+++ lambda_functions/tre-bagit-to-dri-sip/tre_bagit_to_dri_sip.py
@@ -7,6 +7,7 @@ from s3_lib import common_lib
 from s3_lib import checksum_lib
 from s3_lib import object_lib
 from s3_lib import tar_lib
+from tre_event_lib import tre_event_api
 
 # Set global logging options; AWS environment may override this though
 logging.basicConfig(
@@ -21,46 +22,42 @@ logger.setLevel(logging.INFO)
 # Get environment variable values
 env_out_bucket = common_lib.get_env_var('S3_DRI_OUT_BUCKET', must_exist=True, must_have_value=True)
 env_tre_presigned_url_expiry = common_lib.get_env_var('TRE_PRESIGNED_URL_EXPIRY', must_exist=True, must_have_value=True)
+env_process = common_lib.get_env_var('TRE_PROCESS_NAME', must_exist=True, must_have_value=True)
+env_producer = common_lib.get_env_var('TRE_SYSTEM_NAME', must_exist=True, must_have_value=True)
+env_environment = common_lib.get_env_var('TRE_ENVIRONMENT', must_exist=True, must_have_value=True)
 
-KEY_NUM_RETRIES='number-of-retries'
-KEY_ERROR='error'
-KEY_ERROR_MESSAGE='error-message'
-KEY_S3_BUCKET='s3-bucket'
-KEY_OUTPUT_MESSAGE='output-message'
-KEY_SIP_ZIP_URL="sip-zip-url"
-KEY_SIP_ZIP_URL_SHA256="sip-zip-sha256-url"
+KEY_S3_OBJECT_ROOT = 's3-object-root'
+KEY_S3_FOLDER_URL = 's3-folder-url'
+KEY_S3_SHA256_URL = 's3-sha256-url'
+KEY_NUMBER_OF_RETRIES = 'number-of-retries'
+KEY_FILE_TYPE = 'file-type'
+
+EVENT_NAME_INPUT = 'bagit-validated'
+EVENT_NAME_OUTPUT_OK = 'dri-preingest-sip-available'
+EVENT_NAME_OUTPUT_ERROR = 'dri-preingest-sip-error'
 
 
 def handler(event, context):
     """
-    Given a bagit unzip sitting in the s3-bucket provided in event object of handler + location built from the event's
-    type/consignemnt_ref/retry_number then a dri-sip is built in bucket provided in env variable S3_DRI_OUT_BUCKET at
-    type/consignemnt_ref/retry_number/sip
+    Given a bagit unzip sitting at event's "s3_object_root" then a dri-sip is provided in env var S3_DRI_OUT_BUCKET
     """
     logger.info(f'handler start: event="{event}"')
 
-    # Output data
-    output = {
-        KEY_ERROR: False,
-        KEY_OUTPUT_MESSAGE: None
-    }
+    tre_event_api.validate_event(event=event, schema_name=EVENT_NAME_INPUT)
 
     try:
         # Get input parameters from Lambda function's event object
-        s3_data_bucket = event['parameters']['bagit-validated']['s3-bucket']
-        consignment_reference = event['parameters']['bagit-validated']['reference']
-        consignment_type = event['producer']['type']
-        s3_object_root = event['parameters']['bagit-validated']['s3-object-root']
-
-        # Create event copy
-        output_event = event.copy()
-        logger.info(f'output_event={output_event}')
-        output[KEY_OUTPUT_MESSAGE] = output_event
+        s3_data_bucket = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT][tre_event_api.KEY_S3_BUCKET]
+        consignment_reference = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT][tre_event_api.KEY_REFERENCE]
+        consignment_type = event[tre_event_api.KEY_PRODUCER][tre_event_api.KEY_TYPE]
+        s3_object_root = event[tre_event_api.KEY_PARAMETERS][EVENT_NAME_INPUT][KEY_S3_OBJECT_ROOT]
 
         logger.info(
             f'consignment_reference="{consignment_reference}" '
             f'consignment_type="{consignment_type}" '
-            f's3_data_bucket="{s3_data_bucket}" ')
+            f's3_data_bucket="{s3_data_bucket}" '
+            f's3_object_root="{s3_object_root}" '
+        )
         # set-up config_dicts x 3 & make bagit data
         s3c = s3_config_dict(s3_object_root)
         bc = bagit_config_dict(consignment_reference)
@@ -111,17 +108,49 @@ def handler(event, context):
             bucket=env_out_bucket,
             key=sip_zip_key + '.sha256',
             expiry=env_tre_presigned_url_expiry)
-        output_event[KEY_SIP_ZIP_URL] = presigned_tar_gz_url
-        output_event[KEY_SIP_ZIP_URL_SHA256] = presigned_checksum_url
+
+        output_parameter_block = {
+            EVENT_NAME_OUTPUT_OK: {
+                tre_event_api.KEY_REFERENCE: consignment_reference,
+                KEY_S3_FOLDER_URL: presigned_tar_gz_url,
+                KEY_S3_SHA256_URL: presigned_checksum_url,
+                KEY_NUMBER_OF_RETRIES: 0,
+                KEY_FILE_TYPE:  "TAR"
+            }
+        }
+
+        event_output_ok = tre_event_api.create_event(
+            environment=env_environment,
+            producer=env_producer,
+            process=env_process,
+            event_name=EVENT_NAME_OUTPUT_OK,
+            prior_event=event,
+            parameters=output_parameter_block
+        )
+
+        logger.info(f'event_output_ok:\n%s\n', event_output_ok)
+        return event_output_ok
 
     except ValueError as e:
-        logging.error(f'handler error: {str(e)}')
-        output[KEY_ERROR] = True
-        output[KEY_ERROR_MESSAGE] = str(e)
+        logging.error('handler error: %s', str(e))
+        output_parameter_block = {
+            EVENT_NAME_OUTPUT_ERROR: {
+                tre_event_api.KEY_REFERENCE: consignment_reference,
+                tre_event_api.KEY_ERRORS: [str(e)]
+            }
+        }
 
-    # Set output data
-    logger.info('handler return')
-    return output
+        event_output_error = tre_event_api.create_event(
+            environment=env_environment,
+            producer=env_producer,
+            process=env_process,
+            event_name=EVENT_NAME_OUTPUT_ERROR,
+            prior_event=event,
+            parameters=output_parameter_block
+        )
+
+        logger.info(f'event_output_error:\n%s\n', event_output_error)
+        return event_output_error
 
 
 def dri_config_dict(consignment_reference, consignment_series):
@@ -161,10 +190,9 @@ def bagit_config_dict(consignment_reference):
 
 def s3_config_dict(s3_object_root):
     sip_directory = '/sip/'
-    prefix_to_bagit = s3_object_root
     return dict(
-        PREFIX_TO_BAGIT=prefix_to_bagit,
-        PREFIX_TO_SIP=prefix_to_bagit + sip_directory
+        PREFIX_TO_BAGIT=s3_object_root,
+        PREFIX_TO_SIP=s3_object_root + sip_directory
     )
 
 
--- lambda_functions/tre-bagit-to-dri-sip/version.sh
+++ lambda_functions/tre-bagit-to-dri-sip/version.sh
@@ -4,4 +4,5 @@ docker_image_tag=0.0.3
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
 # shellcheck disable=SC2034  # var imported elsewhere
-lib_build_list=('s3_lib' 'lib/tre_lib')
+lib_build_list=('s3_lib')
+tre_event_lib_build_tag=0.0.6-alpha
