8bbea33966ea90d99b0a73532585202dcc8f0cc2
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-09-30 11:24:47 +0100

    Removed tre-vb-trigger (use shared tre-sqs-sf-trigger)

--- lambda_functions/tre-vb-trigger/version.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker_image_name=tre-vb-trigger
-docker_image_tag=2.0.6
-# shellcheck disable=SC2034  # var imported elsewhere
-docker_image="${docker_image_name}":"${docker_image_tag}"
