675bca24502f911f652f41fbe74fdc90377d39c3
rahmedzaizi <rahmed@zaizi.com>
2022-10-04 15:37:07 +0100

    fixed key errors

--- lambda_functions/tre-dlq-slack-alerts/tre_dlq_slack_alerts.py
+++ lambda_functions/tre-dlq-slack-alerts/tre_dlq_slack_alerts.py
@@ -13,23 +13,27 @@ def lambda_handler(event, context):
 
     url = os.environ["SLACK_WEBHOOK_URL"]
     env = os.environ["ENV"]
-
+    
     records = event["Records"][0]
     print(records)
+    
+    attributes = records["attributes"]
 
     body = json.loads(records["body"])
     message = body["Message"]
-    source_lambda = records["eventSourceARN"].split(":")[-1]
+    print(message)
+    
+    source_queue = attributes["DeadLetterQueueSourceArn"].split(":")[-1]
 
+    final_message = f"*STATUS* :red_circle: \n  *Environment* `{env}` \n There is a message in `{source_queue}` which lambda failed to consume. \n ```{message}```\n"
+    
     msg = {
-        "Dead Letter in Queue\n"
         "channel": os.environ["SLACK_CHANNEL"],
         "username": os.environ["SLACK_USERNAME"],
-        "Environment": env,
-        "text": message,
-        "lambda": source_lambda
+        "text": final_message
     }
 
+    print(final_message)
     encoded_msg = json.dumps(msg, indent=2).encode("utf-8")
     resp = http.request("POST", url, body=encoded_msg)
-    print({"message": message, "status_code": resp.status, "response": resp.data})
+    print({"status_code": resp.status, "response": resp.data})
--- lambda_functions/tre-dlq-slack-alerts/version.sh
+++ lambda_functions/tre-dlq-slack-alerts/version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker_image_name=tre-dlq-slack-alerts
-docker_image_tag=0.0.01
+docker_image_tag=0.0.1
 # shellcheck disable=SC2034  # var imported elsewhere
 docker_image="${docker_image_name}":"${docker_image_tag}"
