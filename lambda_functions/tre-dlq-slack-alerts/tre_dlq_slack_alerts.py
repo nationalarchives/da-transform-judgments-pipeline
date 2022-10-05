5ee49b856787b68688ba11e704364a9c7ddd471d
rahmedzaizi <rahmed@zaizi.com>
2022-10-05 11:32:16 +0100

    improved python

--- lambda_functions/tre-dlq-slack-alerts/tre_dlq_slack_alerts.py
+++ lambda_functions/tre-dlq-slack-alerts/tre_dlq_slack_alerts.py
@@ -3,29 +3,22 @@ import json
 import os
 
 http = urllib3.PoolManager()
-
+url = os.environ["SLACK_WEBHOOK_URL"]
+env = os.environ["ENV"]
 
 def lambda_handler(event, context):
     """
     Pushes dead letter notifications to slack, alongside the environment, execution,
      and the triggering SQS dead letter event
     """
-
-    url = os.environ["SLACK_WEBHOOK_URL"]
-    env = os.environ["ENV"]
-    
     records = event["Records"][0]
     print(records)
-    
     attributes = records["attributes"]
-
-    body = json.loads(records["body"])
-    message = body["Message"]
-    print(message)
-    
+    body = records["body"]
+    print(body)
     source_queue = attributes["DeadLetterQueueSourceArn"].split(":")[-1]
 
-    final_message = f"*STATUS* :red_circle: \n  *Environment* `{env}` \n There is a message in `{source_queue}` which lambda failed to consume. \n ```{message}```\n"
+    final_message = f"*STATUS* :red_circle: \n  *Environment* `{env}` \n There is a message in `{source_queue}` which lambda failed to consume. \n ```{body}```\n"
     
     msg = {
         "channel": os.environ["SLACK_CHANNEL"],
